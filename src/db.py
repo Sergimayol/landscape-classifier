d60c488b35415f67fecc657773e05336019fe397
Sergimayol <sergi2808@gmail.com>
2023-11-03 17:52:17 +0100

    multiple db opt

--- src/db.py
+++ src/db.py
@@ -1,8 +1,9 @@
 import io
 import sqlite3
 import numpy as np
+import mysql.connector
 from typing import Tuple
-from settings import DB_PATH, USE_DB
+from settings import DB_PATH, MYSQL_HOST, MYSQL_PASSWORD, MYSQL_USER, USE_DB, DB_IS_LOCAL
 
 
 def adapt_array(arr) -> sqlite3.Binary:
@@ -27,7 +28,7 @@ def allow_numpy():
         sqlite3.register_converter("nparray", convert_array)
 
 
-def save_to_db(
+def __save_to_local_db(
     name: str,
     best_params: dict,
     best_score: float,
@@ -40,10 +41,6 @@ def save_to_db(
     confusion_matrix: np.ndarray = None,
     is_val: bool = True,
 ):
-    """Save the given info to the database."""
-    if not USE_DB:
-        return
-
     conn = sqlite3.connect(DB_PATH)
     cursor = conn.cursor()
     cursor.execute(
@@ -86,3 +83,89 @@ def save_to_db(
     conn.commit()
     cursor.close()
     conn.close()
+
+
+def __save_to_remote_db(
+    name: str,
+    best_params: dict,
+    best_score: float,
+    precision: float,
+    recall: float,
+    f1_score: float,
+    support: float,
+    img_size: Tuple[int, int],
+    classification_report: str = None,
+    confusion_matrix: np.ndarray = None,
+    is_val: bool = True,
+):
+    conn = mysql.connector.connect(host=MYSQL_HOST, user=MYSQL_USER, password=MYSQL_PASSWORD)
+    cursor = conn.cursor()
+    cursor.execute("CREATE DATABASE IF NOT EXISTS svm;")
+    cursor.execute("USE svm;")
+    cursor.execute(
+        """
+        CREATE TABLE IF NOT EXISTS svm_train (
+            id INTEGER PRIMARY KEY AUTO_INCREMENT,
+            name VARCHAR(255) NOT NULL,
+            best_params TEXT NOT NULL,
+            best_score REAL NOT NULL,
+            precision REAL NOT NULL,
+            recall REAL NOT NULL,
+            f1_score REAL NOT NULL,
+            support REAL NOT NULL,
+            img_size TEXT NOT NULL,
+            classification_report TEXT,
+            confusion_matrix LONGBLOB,
+            is_val BOOLEAN NOT NULL
+        );
+        """
+    )
+    cursor.execute(
+        """
+        INSERT INTO svm_train (name, best_params, best_score, precision, recall, f1_score, support, img_size, classification_report, is_val)
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s);
+        """,
+        (
+            name,
+            str(best_params),
+            best_score,
+            precision,
+            recall,
+            f1_score,
+            support,
+            str(img_size),
+            str(classification_report),
+            confusion_matrix.dumps(),
+            int(is_val),
+        ),
+    )
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+
+def save_to_db(
+    name: str,
+    best_params: dict,
+    best_score: float,
+    precision: float,
+    recall: float,
+    f1_score: float,
+    support: float,
+    img_size: Tuple[int, int],
+    classification_report: str = None,
+    confusion_matrix: np.ndarray = None,
+    is_val: bool = True,
+):
+    """Save the given info to the database."""
+    if not USE_DB:
+        return
+
+    if DB_IS_LOCAL:
+        __save_to_local_db(
+            name, best_params, best_score, precision, recall, f1_score, support, img_size, classification_report, confusion_matrix, is_val
+        )
+    else:
+        __save_to_remote_db(
+            name, best_params, best_score, precision, recall, f1_score, support, img_size, classification_report, confusion_matrix, is_val
+        )
--- src/settings.py
+++ src/settings.py
@@ -15,7 +15,11 @@ OUTPUT_PATH = os.path.join("..", "out")
 # Other info
 USE_CACHE = True
 USE_DB = True
+DB_IS_LOCAL = True
 WORKERS = -1
 
 # Database info
 DB_PATH = os.path.join("..", "out", "svm.sqlite3")
+MYSQL_HOST = os.environ.get("MYSQL_HOST", "localhost")
+MYSQL_USER = os.environ.get("MYSQL_USER", "root")
+MYSQL_PASSWORD = os.environ.get("MYSQL_PASSWORD", "root")
